d1176c96f84b1940458a37f476736b28bda1b5d9
NatnaelTaddese <natnaeltaddese3@gmail.com>
2025-03-21 11:27:20 +0800

    Refactor Section component and add Hero component with scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useRef } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import Section from "./components/Section";
-import { motion, useScroll, useTransform, useInView } from "motion/react";
+import Section from "./components/atomic/Section";
 
 // Images
 import FigureOne from "./assets/backgrounds/Figure-01.jpg";
@@ -14,15 +12,9 @@ import FigureSix from "./assets/backgrounds/Figure-06.jpg";
 import FigureSeven from "./assets/backgrounds/Figure-07.jpg";
 import FigureEight from "./assets/backgrounds/Figure-08.jpg";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
-  const { scrollYProgress } = useScroll();
-  const x_main_title = useTransform(scrollYProgress, [0, 1], [0, -1000]);
-  const x_minor_title = useTransform(scrollYProgress, [0, 1], [0, 1000]);
-
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-
   return (
     <>
       <div className="noise-overlay"></div>
@@ -32,35 +24,7 @@ function App() {
         id={"home"}
         customStyles={{ height: "150vh" }}
       >
-        <div className="hero-content">
-          <div className="hero-container">
-            <motion.h1
-              className="title-text"
-              style={{ x: x_main_title }}
-              ref={ref}
-              initial={{ opacity: 0, y: 10 }} // Start invisible & off-screen right
-              animate={isInView ? { opacity: 1, y: 0 } : {}} // Animate when in view
-              transition={{ duration: 0.8, ease: "easeOut" }}
-            >
-              Robel T. Sebsibe
-            </motion.h1>
-            <h2 className="title-text-aka">
-              {/* <span>aka</span> */}
-              <motion.span
-                data-text="Dotphic"
-                className="title-text-comic"
-                style={{ x: x_minor_title }}
-                ref={ref}
-                initial={{ opacity: 0, x: 100 }} // Start invisible & off-screen right
-                animate={isInView ? { opacity: 1, x: 0 } : {}} // Animate when in view
-                transition={{ duration: 5, ease: "easeOut" }}
-              >
-                Dotphic
-              </motion.span>
-            </h2>
-            <button className="button-cta">About</button>
-          </div>
-        </div>
+        <Hero />
       </Section>
       <Section background={FigureFour} id={"about"}></Section>
       <Section background={FigureSix}></Section>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,45 @@
+import "./Hero.css";
+
+import { useRef } from "react";
+import { motion, useScroll, useTransform, useInView } from "motion/react";
+
+export default function Hero() {
+  const { scrollYProgress } = useScroll();
+  const x_main_title = useTransform(scrollYProgress, [0, 1], [0, -1000]);
+  const x_minor_title = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <div className="hero-content">
+      <div className="hero-container">
+        <motion.h1
+          className="title-text"
+          style={{ x: x_main_title }}
+          ref={ref}
+          initial={{ opacity: 0, y: 10 }} // Start invisible & off-screen right
+          animate={isInView ? { opacity: 1, y: 0 } : {}} // Animate when in view
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          Robel T. Sebsibe
+        </motion.h1>
+        <h2 className="title-text-aka">
+          {/* <span>aka</span> */}
+          <motion.span
+            data-text="Dotphic"
+            className="title-text-comic"
+            style={{ x: x_minor_title }}
+            ref={ref}
+            initial={{ opacity: 0, x: 100 }} // Start invisible & off-screen right
+            animate={isInView ? { opacity: 1, x: 0 } : {}} // Animate when in view
+            transition={{ duration: 5, ease: "easeOut" }}
+          >
+            Dotphic
+          </motion.span>
+        </h2>
+        <button className="button-cta">About</button>
+      </div>
+    </div>
+  );
+}
